62b78a6f6201aa7211640bda4ac5399a2fcbf241
rfrat <r.fratrik7@gmail.com>
2026-05-08 16:52:43 +0200

    update project configurations

--- OrganizationAPI/Program.cs
+++ OrganizationAPI/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using OrganizationAPI.Data;
+using OrganizationAPI.Services;
+using OrganizationAPI.Services.Interfaces;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,12 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
-
-//pridane
 builder.Services.AddDbContext<AppDbContext>(options =>
 	options.UseSqlServer(
 		builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<ICompanyService, CompanyService>();
+builder.Services.AddScoped<IOrganizationUnitService, OrganizationUnitService>();
+var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
@@ -22,11 +27,11 @@ using (var scope = app.Services.CreateScope())
 	dbContext.Database.Migrate();
 	await DatabaseSeeder.SeedAsync(dbContext);
 }
-//potialto
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.MapOpenApi();
+	app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
@@ -34,5 +39,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+//app.MapGet("/", () => "Organization API is running");
 app.Run();
