5d401c640ae1b7553eb8687a98546ea9c009a4b6
rfrat <r.fratrik7@gmail.com>
2026-05-08 16:45:35 +0200

    DbContextFactory

---
+++ OrganizationAPI/Data/AppDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace OrganizationAPI.Data
+{
+	public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+	{
+		public AppDbContext CreateDbContext(string[] args)
+		{
+			var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+			optionsBuilder.UseSqlServer(
+				"Server=localhost,1433;Database=OrganizationDb;User Id=sa;Password=asdfghjkl_123;TrustServerCertificate=True;");
+
+			return new AppDbContext(optionsBuilder.Options);
+		}
+	}
+}
